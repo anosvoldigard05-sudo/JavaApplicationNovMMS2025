8390c2b1b5dce0a7fa4fd2606e45293f64fee5f4
anosvoldigard05-sudo <anosvoldigard05@gmail.com>
2026-06-03 10:20:22 +0100

    Output

---
+++ Output.java
@@ -0,0 +1,13 @@
+public class Output{
+    public static void main(String[] args){
+	System.out.println("Hello everyone, this is my first Java code");
+	System.out.println("Learning java is fun");
+	System.out.print("Here I am using the print method. ");
+	System.out.print("This text will be appended");
+	System.out.printf("%n Mercy Ben is ,y2f%n',old.%n",25);
+	System.out.printf("My account balance is %f%n",7654.922276);
+	System.out.printf("%s is a boy","Benjamin");
+	System.out.printf("Do you love java?%b%n",true);
+	System.out.Printf("Fuel price in nigeria has been increased by %%80In");
+	}
+}
---
+++ Question.java
@@ -0,0 +1,5 @@
+public class Question {
+    public Static void main(String[] args){
+	System.out.println("What is your name and who are you?");
+    }
+}
