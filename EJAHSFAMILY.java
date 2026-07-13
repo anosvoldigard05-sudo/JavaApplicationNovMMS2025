89fba34f202b288eaaa3c3adc1c6c7d658b7a3b7
anosvoldigard05-sudo <anosvoldigard05@gmail.com>
2026-07-13 17:09:54 +0100

    MultipleSwitchCaseStatement

---
+++ EJAHSFAMILY.java
@@ -0,0 +1,16 @@
+public class EJAHSFAMILY{
+	public static void main(String[] args){
+		
+		System.out.println("Divine: im the best in freefire");
+		System.out.println("=========================================");
+		System.out.println("Naruto: dey play i am the best currently i have surpassed you");
+		System.out.println("===========================================");
+		System.out.println("Divine: just dey play enter game make i beat you for 1v1 make i thrash you");
+		System.out.println("===========================================");
+		System.out.println("Naruto: i dey enter game make i show you why they call me naruto");
+		System.out.println("===========================================");
+		System.out.println("Divine: HAHAHA dey play im ready for you today");
+		System.out.println("===========================================");
+	}
+}	
+		
